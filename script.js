62260aa186cb808a95911210e5c524db876ca867
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-18 16:06:33 +0100

    [UPD] add notes - Esercizio Terminato

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
+// creato ciclo for;
 for (let i=1; i<=100; i=i+1){
-
+    // primo if che stabilisce se i è divisibile per 3 e 5;
     if(i % 3 == 0 && i % 5 == 0){
-        console.log(`${i} FizzBuzz`)
+        console.log(`${i} FizzBuzz`);
     }
-
+    // secondo if che stabilisce se i è divisibile per 3;
     else if(i % 3 == 0){
-        console.log(`${i} Fizz`)
+        console.log(`${i} Fizz`);
     }
-
+    // terzo if che stabilisce se i è divisibile per 5;
     else if(i % 5 == 0){
-        console.log(`${i} Buzz`)
+        console.log(`${i} Buzz`);
     }
-
+    // stampa del numero non divisibile per i valori sopracitati;
     else{
-        console.log(i)
+        console.log(i);
     }
 }
